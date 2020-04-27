b96ebbe85c10abe137ef6d2ece93d35a9790c0bc
Sulabh Shrestha <sulabh.shr@gmail.com>
2020-04-26 22:21:57 -0400

    Remove redundant parameters Add test dataset

--- parameters.py
+++ parameters.py
@@ -3,6 +3,5 @@ curr_dir = os.getcwd()
 annotation_path = os.path.join(curr_dir,"state-farm-distracted-driver-detection", "driver_imgs_list.csv")
 train_dir = os.path.join(curr_dir, "state-farm-distracted-driver-detection", "imgs", "train")
 test_dir = os.path.join(curr_dir, "state-farm-distracted-driver-detection", "imgs", "test")
-
-T = 5
 n_classes = 10
+
