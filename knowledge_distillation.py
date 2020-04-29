240890bf4c47b85ea8f700f72e76464dba5d42ba
Sulabh Shrestha <sulabh.shr@gmail.com>
2020-04-29 12:47:44 -0400

    Add test dataset and update training
    Move hyperparamters inside respective function
    Move load_data function to dataset.py
    Add best val model saving

--- knowledge_distillation.py
+++ knowledge_distillation.py
@@ -1,19 +1,16 @@
-from resnet_18_implement import load_data, train_resnet
-from parameters import *
-from dataset import *
+import tqdm
+import numpy as np
+from datetime import datetime
+
 import torch
 import torch.nn as nn
 from torch import optim
 import torch.nn.functional as F
-import tqdm
-from datetime import datetime
 import torchvision.models as models
 from torch.utils.tensorboard import SummaryWriter
 
-
-batch_size = 5
-num_epochs = 100
-learning_rate = 0.0001
+from dataset import load_data
+from parameters import *
 
 
 class Student_network(nn.Module):
@@ -41,17 +38,42 @@ class Student_network(nn.Module):
         return x
 
 
+class softCrossEntropy(nn.Module):
+    def __init__(self):
+        super(softCrossEntropy, self).__init__()
+        return
+
+    def forward(self, inputs, target):
+        log_likelihood = - F.log_softmax(inputs, dim=1)
+        sample_num, class_num = target.shape
+        loss = torch.sum(torch.mul(log_likelihood, target))/sample_num
+        return loss
+
+
 def train_student_plain():
+
+    batch_size = 32
+    num_epochs = 200
+    learning_rate = 0.0001
+    prev_t_avg_loss = float('inf')
+
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     model  = Student_network()
     model = model.to(device)
-    train_loader, val_loader = load_data()
+    train_loader, val_loader = load_data(batch_size)
     criterion = nn.NLLLoss()
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-    run_time = datetime.now().strftime(("%m-%d %H-%M"))
-    writer = SummaryWriter(os.path.join('runs', "student_" + run_time))
+
+    run_time = datetime.now().strftime(("%m-%d_%H-%M"))
+    writer = SummaryWriter(os.path.join('runs', 'student_'+run_time))
 
     for epoch in range(1, num_epochs + 1):
+        if epoch == 50:
+            learning_rate = 0.5 * learning_rate
+
+        if epoch == 125:
+            learning_rate = 0.1 * learning_rate
+
         print(f'Starting epoch {epoch}')
         model.train()
 
@@ -67,6 +89,9 @@ def train_student_plain():
             accuracy = (pred_max == labels).sum().item() / pred_max.size()[0]
             writer.add_scalar('eval_train_acc', accuracy, (epoch - 1) * len(train_loader) + i)
             writer.add_scalar('eval_train_loss', loss.item(), (epoch - 1) * len(train_loader) + i)
+
+        print('Saving the model')
+        torch.save(model.state_dict(), os.path.join('models', f'student_{run_time}.pt'))
 
         # Validate model
         model.eval()
@@ -92,38 +117,41 @@ def train_student_plain():
         writer.add_scalar('eval_val_loss', t_avg_loss, epoch - 1)
         writer.add_scalar('eval_val_acc', t_avg_acc, epoch - 1)
         print('Saving the model')
-        torch.save(model.state_dict(), os.path.join('models', f'student_{T}_{run_time}.pt'))
+        torch.save(model.state_dict(), os.path.join('models', f'student_{run_time}.pt'))
 
-
-class softCrossEntropy(nn.Module):
-    def __init__(self):
-        super(softCrossEntropy, self).__init__()
-        return
-
-    def forward(self, inputs, target):
-        log_likelihood = - F.log_softmax(inputs, dim=1)
-        sample_num, class_num = target.shape
-        loss = torch.sum(torch.mul(log_likelihood, target))/sample_num
-        return loss
+        # Check for best validation loss
+        if t_avg_loss < prev_t_avg_loss:
+            print('Saving best val loss model')
+            torch.save(model.state_dict(), os.path.join('models', f'student_{run_time}_val.pt'))
+            prev_t_avg_loss = t_avg_loss
 
 
 def train_student_distilled():
+
+    batch_size = 32
+    num_epochs = 200
+    learning_rate = 0.0001
+    T = 3
+    teacher_path = os.path.join(curr_dir, 'models', "resnet18_3_04-26 22-38.pt")
+    prev_t_avg_hard_loss = float('inf')
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
     teacher_network = models.resnet18(pretrained=False)
     num_dim = teacher_network.fc.in_features
     teacher_network.fc = nn.Linear(num_dim, 10)
-    teacher_network.load_state_dict(torch.load(os.path.join(curr_dir, 'models', "resnet18_3_04-26 22-38.pt")))
+    teacher_network.load_state_dict(torch.load(teacher_path))
     teacher_network.eval()
     teacher_network = teacher_network.to(device)
 
     model = Student_network()
     model = model.to(device)
-    train_loader, val_loader = load_data()
+    train_loader, val_loader = load_data(batch_size)
     criterion_hard = nn.NLLLoss()
     criterion_soft = softCrossEntropy()
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-    run_time = datetime.now().strftime(("%m-%d %H-%M"))
-    writer = SummaryWriter(os.path.join('runs', "distilled_"+run_time))
+    run_time = datetime.now().strftime('%m-%d %H-%M')
+
+    writer = SummaryWriter(os.path.join('runs', 'distilled_'+run_time))
 
     for epoch in range(1, num_epochs + 1):
         print(f'Starting epoch {epoch}')
@@ -152,9 +180,13 @@ def train_student_distilled():
             total_loss.backward()
             optimizer.step()
             accuracy = (pred_max == labels).sum().item() / pred_max.size()[0]
+
             writer.add_scalar('eval_train_acc', accuracy, (epoch - 1) * len(train_loader) + i)
             writer.add_scalar('eval_train_loss', loss_hard.item(), (epoch - 1) * len(train_loader) + i)
-            writer.add_scalar('eval_soft_loss', loss_soft.item(), (epoch - 1) * len(train_loader) + i)
+            writer.add_scalar('eval_train_soft_loss', loss_soft.item(), (epoch - 1) * len(train_loader) + i)
+
+        print('Saving the model')
+        torch.save(model.state_dict(), os.path.join('models', f'distilled_{T}_{run_time}.pt'))
 
         # Validate model
         model.eval()
@@ -178,21 +210,26 @@ def train_student_distilled():
                 t_total_loss = t_loss_hard + t_loss_soft
                 t_accuracy = (t_pred_max == t_labels).sum().item() / t_pred_max.size()[0]
                 t_acc.append(t_accuracy)
-                t_losses_hard.append(t_loss_hard)
-                t_losses_soft.append(t_loss_soft)
+                t_losses_hard.append(t_loss_hard.item())
+                t_losses_soft.append(t_loss_soft.item())
 
         t_avg_acc = np.mean(t_acc)
         t_avg_hard_loss = np.mean(t_losses_hard)
         t_avg_soft_loss = np.mean(t_losses_soft)
 
+        print(f'Validation loss at the end of epoch {epoch}     : {t_avg_hard_loss:.4f}')
+        print(f'Validation accuracy at the end of epoch {epoch} : {t_avg_acc:.4f}')
 
-        print(f'Validation loss at the end of epoch {epoch}', t_avg_hard_loss)
-        print(f'Validation accuracy at the end of epoch {epoch}', t_avg_acc)
         writer.add_scalar('eval_val_loss', t_avg_hard_loss, epoch - 1)
         writer.add_scalar('eval_val_acc', t_avg_acc, epoch - 1)
         writer.add_scalar('eval_val_soft_loss', t_avg_soft_loss, epoch - 1)
-        print('Saving the model')
-        torch.save(model.state_dict(), os.path.join('models', f'distilled_{T}_{run_time}.pt'))
+        writer.add_scalar('eval_val_total_loss', t_total_loss, epoch-1)
+
+        # Check for best validation loss
+        if t_avg_hard_loss < prev_t_avg_hard_loss:
+            print('Saving best val loss model')
+            torch.save(model.state_dict(), os.path.join('models', f'distilled_{T}_{run_time}_val.pt'))
+            prev_t_avg_hard_loss = t_avg_hard_loss
 
 
 if __name__ == '__main__':
